ff163e874a51f27d18f0c00b894d23a4820a1598
Kamil Kruczek <kamil.kruczek@o2.pl>
2019-01-08 20:06:53 +0100

    :sparkles: Add HireMe component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.scss';
 // import PageChat from './components/PageChat/PageChat';
 import Header from './components/Header/Header';
 import AboutMe from './components/AboutMe/AboutMe';
+import HireMe from './components/HireMe/HireMe';
 
 const App = () => (
   <div className="App">
     {/* <PageChat/> */}
     <Header/>
     <AboutMe/>
+    <HireMe/>
   </div>
 );
 
