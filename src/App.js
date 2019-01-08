45639a7e8cae505e342cca4e91313bb8e017ce17
Kamil Kruczek <kamil.kruczek@o2.pl>
2019-01-08 19:55:43 +0100

    :sparkles: Add AboutMe component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.scss';
 // import PageChat from './components/PageChat/PageChat';
 import Header from './components/Header/Header';
+import AboutMe from './components/AboutMe/AboutMe';
 
 const App = () => (
   <div className="App">
     {/* <PageChat/> */}
     <Header/>
+    <AboutMe/>
   </div>
 );
 
---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  width: 100%;
+  background: #FFFFFF;
+`;
+
+const About = styled.div`
+  max-width: 980px;
+  margin: 0 auto;
+  
+  display: flex;
+  flex-direction: column;
+  
+  padding-bottom: 170px;
+`;
+
+const Caption = styled.h2`
+  position: relative;
+  max-width: 720px;
+  color: #0B1C4C;
+  font-family: Roboto-Light, sans-serif;
+  font-weight: 300;
+  font-size: 36px;
+  line-height: 54px;
+  margin-top: -80px;
+  
+  &:after {
+    content: '';
+    position: absolute;
+    height: 6px;
+    bottom: -20px;
+    left: 0;
+    width: 50px;
+    border-radius: 3px;
+    background: #235EFF;
+    background-image: -webkit-gradient(linear, 0 0, 100% 0, from(#4997FF), to(#235EFF));
+    background-image: -webkit-linear-gradient(left, #4997FF, #235EFF);
+    background-image: -moz-linear-gradient(left, #4997FF, #235EFF);
+    background-image: -o-linear-gradient(left, #4997FF, #235EFF);
+    box-shadow: 0 3px 8px 0 rgba(35,94,255,0.1);
+  }
+`;
+
+const Paragraph = styled.p`
+  max-width: 750px;
+  color: #0B1C4C;
+  font-family: Roboto-Regular, sans-serif;
+  font-size: 16px;
+  line-height: 24px;
+  opacity: 0.6;
+`;
+
+const AboutMe = () => (
+  <Wrapper>
+    <About>
+      <Caption>
+        {'Mobile first web development with HTML, CSS, JavaScript and React.'}
+      </Caption>
+      <Paragraph>
+        {'My name is Kamil Kruczek. I\'m a freelance frontend web developer based in Poland and I\'d like to help you build your next project.'}
+      </Paragraph>
+    </About>
+  </Wrapper>
+);
+
+export default AboutMe;
